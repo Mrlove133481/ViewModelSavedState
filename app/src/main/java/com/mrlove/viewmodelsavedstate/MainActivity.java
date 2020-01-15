6da66f4e8932ab517163c823719face15916f571
Mrlove133481 <1334819077@qq.com>
2020-01-15 11:33:51 +0800

    通过使用SavedstateHandle实现布局重新加载数据的保存

--- app/src/main/java/com/mrlove/viewmodelsavedstate/MainActivity.java
+++ app/src/main/java/com/mrlove/viewmodelsavedstate/MainActivity.java
@@ -1,14 +1,28 @@
 package com.mrlove.viewmodelsavedstate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import com.mrlove.viewmodelsavedstate.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    public final static String KEY_NUMBER = "my_number";
+    MyViewModel viewModel;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MyViewModel.class);
+        binding.setData(viewModel);
+        binding.setLifecycleOwner(this);
     }
 }
